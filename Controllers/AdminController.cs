2114aa58b6b40b7457312f1aada63317191bf7b5
fatlc24 <nghiemquanganh2014@gmail.com>
2024-04-16 23:03:01 +0700

    admin

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,6 +1,8 @@
 ﻿using BTLwebNC.Models;
 using BTLwebNC.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using NuGet.Protocol.Core.Types;
 using System.Security.Claims;
 
 namespace BTLwebNC.Controllers
@@ -83,7 +85,65 @@ namespace BTLwebNC.Controllers
         }
         public IActionResult AdminNews()
         {
+            var rusual = adminRepositony.GetAllNews();
+            return View(rusual);
+        }
+        public IActionResult createNew()
+        {
+
             return View();
+        }
+        [HttpPost]
+        public IActionResult createNew(TblNews news)
+        {
+            var userClaims = User.Identity as ClaimsIdentity;
+            if (userClaims != null)
+            {
+                // Find the claim by its type (ClaimTypes.NameIdentifier in this case)
+                var idusernameClaim = userClaims.FindFirst(ClaimTypes.NameIdentifier);
+                var tenusername = userClaims.FindFirst("UseName");
+                var name = userClaims.FindFirst("Name");
+                var email = userClaims.FindFirst("Email");
+                var phone = userClaims.FindFirst("Phone");
+                var id_user = userClaims.FindFirst("id_user");
+                if (idusernameClaim != null)
+                {
+                    string username1 = tenusername.Value;
+                    string tendangnhap = name.Value;
+                    string emaildangnhap = email.Value;
+                    string phonedangnhap = phone.Value;
+                    string id = id_user.Value;
+                    TempData["Username"] = username1;
+                    TempData["idLoginData"] = idusernameClaim;
+                    TempData["LoginData_name"] = tendangnhap;
+                    TempData["LoginEmail"] = emaildangnhap;
+                    TempData["LoginPhone"] = phonedangnhap;
+                    TempData["id_user"] = id;
+                    // Now, 'username' contains the value of the Claim with ClaimTypes.NameIdentifier.
+                }
+
+                // You can also access your custom claim ("OtherProperties" in this case) in a similar manner.
+            }
+            adminRepositony.createNew(news);
+            return View();
+        }
+        [HttpDelete("deleteNewByID/{id}")]
+        public IActionResult DeleteById(int id)
+        {
+            adminRepositony.deleteNewByID(id);
+            return Json(new { success = true });
+        }
+        [HttpPost]
+        public IActionResult UpdateNew(int id, TblNews news)
+        {
+            var result = adminRepositony.updateNew(id, news);
+            return RedirectToAction("AdminNews", "Admin");
+        }
+        [HttpGet]
+        public IActionResult UpdateNew(int id)
+        {
+            var result = adminRepositony.getNewById(id);
+            return View(result);
         }
     }
 }
--- obj/Debug/net8.0/BTLwebNC.AssemblyInfo.cs
+++ obj/Debug/net8.0/BTLwebNC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTLwebNC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b1e39d77e6ddffefcccd2ccc65e4f1c7cbf843e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d9aea56c89c2710e85b32d4e34a49962346c680")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTLwebNC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTLwebNC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
